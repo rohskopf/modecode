4c1eef61badd80f88df21167d2a7d83979707644
rohskopf <drew.rohskopf@gmail.com>
2021-07-03 18:06:20 -0400

    Updated view_mode.py to read LAMMPS data files.

--- tools/view_mode.py
+++ tools/view_mode.py
@@ -1,16 +1,18 @@
 import numpy as np
 
-mode = 20
+mode = 1055 # Mode index starting from 1
 
 amp = [0.,100.]
 
 ndisps = 10
 
-emat = np.loadtxt('eigenvectors.csv', delimiter=',')
+emat = np.loadtxt('EMAT')
 
 shape = np.shape(emat)
 
-natoms = shape[0]/3
+#print(shape)
+
+natoms = int(shape[0]/3)
 
 eindx = 0
 
@@ -20,26 +22,65 @@ evecs = []
 for n in range(0,natoms):
     evec = []
     for a in range(0,3):
-        evec.append(emat[eindx+a][mode])
+        evec.append(emat[eindx+a][mode-1])
     eindx += 3
     evecs.append(evec)
 
 evecs = np.array(evecs)
 
-# Read config
 
-fh_config = open('CONFIG','r')
+#Read LAMMPS data file.
+nskip = 0 # Used by np.loadtxt soon.
+fh = open("DATA", 'r')
+# Ignore first line
+line = fh.readline()
+nskip+=1
+# Next line is empty.
+line = fh.readline()
+nskip+=1
+# Get number of atoms.
+line = fh.readline()
+nskip+=1
+natoms = int(line.split()[0])
+print("%d atoms." % (natoms))
+# Find line with atom types
+while ("types" not in line):
+  line = fh.readline()
+  nskip+=1
+ntypes = int(line.split()[0])
+print("%d atom types." % (ntypes))
+# Find masses.
+while ("Masses" not in line):
+  line = fh.readline()
+  nskip+=1
+# Skip next line.
+line = fh.readline()
+nskip+=1
+# Store masses.
+masses_types = []
+for t in range(0,ntypes):
+  line = fh.readline()
+  nskip+=1
+  masses_types.append( float(line.split()[1]) )
+masses_types = np.array(masses_types)
+print("Masses of types:")
+print(masses_types)
+# Find atoms.
+while ("Atoms" not in line):
+  line = fh.readline()
+  nskip+=1
+# Skip next line.
+line = fh.readline()
+nskip+=1
+print("Should skip %d lines to read atoms." % (nskip))
+fh.close()
 
-line = fh_config.readline()
-line = fh_config.readline()
-box = [float(a) for a in line.split()]
-x = []
-types = []
-for n in range(0,natoms):
-    line = fh_config.readline()
-    line_split = [float(a) for a in line.split()]
-    x.append(line_split[1:])
-    types.append(int(line_split[0]))
+data = np.loadtxt('DATA', skiprows=nskip)
+print(data)
+types = data[:,1]
+print(types)
+x = data[:,2:]
+print(x)
 
 # Make xyz
 
@@ -141,7 +182,7 @@ for d in range(0,ndisps-1):
         fh_xyz.write('%d %f %f %f\n' % (types[n], xnew,ynew,znew))
 
 
-fh_config.close()
+#fh_config.close()
 fh_xyz.close() 
 
 
